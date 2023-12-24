6d7ce2033f487b4b49bdca60dcd1a70d55bf08c7
AlecJDavidson <alecjdavidson@outlook.com>
2023-12-23 21:42:05 -0500

    Submission is working again. Time to implement the chat history.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,60 @@
-import './App.css'
+import React, { useState } from 'react';
+import { ollamaRequest } from './ollama/ollamaRequest';
+import { ChatObject, ChatLogObject, ChatLogHistoryObject } from './interfaces/chatInterfaces';
+import { OllamaRequest } from './interfaces/ollamaInterfaces';
+import './App.css';
 
+const App: React.FC = () => {
+  const [prompt, setPrompt] = useState<string>('');
+  const [completion, setCompletion] = useState<string>('');
+  const [chatLog, setChatLog] = useState<ChatLogObject>([]);
 
-const App = () => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPrompt(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (prompt.trim() !== '') {
+      setChatLog([...chatLog, prompt]);
+      let request: OllamaRequest = {
+        model: 'mistral',
+        messages:
+          { "role": "user", "content": prompt }
+
+      }
+
+      ollamaRequest(request);
+      console.log(ollamaRequest);
+      setPrompt('');
+    }
+  };
+
   return (
-  <div>reset</div>
-
-  )
-}
+    <div className="app-container">
+      <div className="box">
+        <div className="chat-log-box">
+          <ul>
+            {chatLog.map((msg, index) => (
+              <li key={index}>{msg}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="Type a prompt to your local LLM..."
+            value={prompt}
+            onChange={handleInputChange}
+            className="text-input"
+          />
+          <button onClick={handleSubmit} className="submit-button">
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
+
