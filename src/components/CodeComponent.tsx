0e7ded1a7ca5280dd2bee9eb2dde8206828539fb
AlecJDavidson <alecjdavidson@outlook.com>
2023-12-24 14:54:32 -0500

    Few more small changes to the look of code blocks.

--- src/components/CodeComponent.tsx
+++ src/components/CodeComponent.tsx
@@ -1,25 +1,52 @@
 // CodeComponent.tsx
-import React from 'react';
+import React, { useRef } from 'react';
+import { FaClipboard } from 'react-icons/fa';
+
 
 interface CodeProps {
   children: React.ReactNode;
 }
 
-const CodeComponent: React.FC<CodeProps> = ({ children }) => (
-  <code
-    style={{
-      fontFamily: '"SFMono-Regular", Menlo, Consolas, "PT Mono", "Liberation Mono", Courier, monospace',
-      lineHeight: 'normal',
-      background: 'rgba(135, 131, 120, 0.15)',
-      color: '#EB5757',
-      borderRadius: '4px',
-      fontSize: '85%',
-      padding: '0.2em 0.4em',
-    }}
-  >
-    {children}
-  </code>
-);
+const CodeComponent: React.FC<CodeProps> = ({ children }) => {
+  const codeRef = useRef<HTMLModElement>(null);
+
+  const handleCopyClick = () => {
+    if (codeRef.current) {
+      const range = document.createRange();
+      range.selectNode(codeRef.current);
+      window.getSelection()?.removeAllRanges();
+      window.getSelection()?.addRange(range);
+      document.execCommand('copy');
+      window.getSelection()?.removeAllRanges();
+    }
+  };
+
+  return (
+    <div>
+      <br/>
+      <code className='code-block'
+        ref={codeRef}
+        style={{
+          fontFamily: '"SFMono-Regular", Menlo, Consolas, "PT Mono", "Liberation Mono", Courier, monospace',
+          lineHeight: 'normal',
+          background: 'rgba(135, 131, 100, 0.15)',
+          color: '#FAF9F6',
+          borderRadius: '4px',
+          fontSize: '90%',
+          padding: '0.2em 0.4em',
+          display: 'block', // Make the code block a block element
+        }}
+      >
+        {children}
+        <button onClick={handleCopyClick} className='copy-clipboard'>
+          <FaClipboard />
+        </button>
+
+        <br /></code><br />
+    </div>
+  );
+};
 
 export default CodeComponent;
+
 
